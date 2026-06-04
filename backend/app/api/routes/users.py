57b04a7966c55d697fb8bb5fc0fa9896cf159288
Adarsh Mishra <adarshmishra@Administrators-Mac-mini.local>
2026-06-04 17:39:43 +0530

    Implement recommendation engine and recommendation API

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from app.schemas.interest_schema import InterestRequest
 from app.core.db import users_collection
+from app.services.recommendation_service import get_recommendation
 
 router = APIRouter(
     prefix= "/user",
@@ -35,4 +36,21 @@ def save_interests(
 
     return {
         "message": "Interests saved successfully"
+    }
+
+@router.get("/recommendation")
+def recommendation(email: str):
+    user = users_collection.find_one(
+        {"email": email}
+    )
+    if not user:
+        raise HTTPException(
+            status_code = 404,
+            detail="User not found"
+        )
+    recommendations = get_recommendation(
+        user["interests"]
+    )
+    return{
+        "recommendations": recommendations
     }
